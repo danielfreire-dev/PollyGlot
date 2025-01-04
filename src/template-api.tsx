f0c507d64091c7e6281b4306d595955a317db432
Daniel Freire <20461343+danielfreire-dev@users.noreply.github.com>
2025-01-04 18:07:42 +0000

    api template file

---
+++ src/template-api.tsx
@@ -0,0 +1,7 @@
+export const OPENAI_API_KEY: string = "YOUR KEY HERE";
+export const OPENAI_ORGANIZATION: string = "YOUR ORGANIZATION HERE";
+export const OPENAI_MODEL: string = "gpt-4-turbo";
+export const OPENAI_TEMPERATURE: number = 0.7;
+export const OPENAI_MAX_TOKENS: number = 2048;
+export const OPENAI_TOP_P: number = 1;
+export const OPENAI_FREQUENCY_PENALTY: number = 0;
